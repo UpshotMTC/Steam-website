4c873df29588e7fb95b927bf29b7c47c5ac70992
UpshotMTC <55609020+UpshotMTC@users.noreply.github.com>
2024-05-02 16:45:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-var http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
+const usrs = []
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080); 
+app.get('/', (req, res) => res.render('index.ejs'))
+
+app.get('/login', (req, res) => res.render('login.ejs'))
+app.post('/login', (req, res) =>{
+
+})
+
+app.get('/register', (req, res) => res.render('register.ejs'))
+app.post('/register', (req, res) =>{
+  
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
